e1a8b79ac76efc627cc5da304c65c1982bb22a6d
Jonathon E <je20k3@gmail.com>
2023-12-03 20:53:47 +0000

    Added userInput object

--- starter/script.js
+++ starter/script.js
@@ -88,21 +88,45 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Var to store user input
-var userInput = [];
+// Create object to store user input
+var userInput = {
+  passLength: 0,
+  charTypes: 0,
 
-// Function to prompt user for password options
-function getPasswordOptions() {
+  charChoices: {
+    lowerCase: { choice: false, prompt: "Include lowercase characters?" },
+    upperCase: { choice: false, prompt: "Include uppercase characters?" },
+    numeric: { choice: false, prompt: "Include numeric characters?" },
+    specialChars: { choice: false, prompt: "Include special characters?" },
+  },
 
+  getUserChoices: function () {
+    this.passLength = prompt("Please enter a password length (min. 8 characters)");
+
+    if (this.passLength >= 8) {
+      for (item in this.charChoices) {
+        this.charChoices[item].choice = confirm(this.charChoices[item].prompt)
+
+        if (this.charChoices[item].choice) {
+          this.charTypes++;
+        } else {
+          console.log(`${this.charChoices[item]}: User selected false`)
+        }
+      }
+    }
+  }
 }
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-  r
+
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  userInput.getUserChoices();
+  console.log(userInput.charOptions);
 
 }
 
@@ -119,3 +143,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+// 1. WHEN user clicks button THEN show LENGTH prompt. IF input is < 8 characters or undefined THEN repeat prompt.
+// 2. IF character type 
